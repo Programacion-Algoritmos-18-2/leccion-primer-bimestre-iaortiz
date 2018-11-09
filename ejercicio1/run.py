7b3e29d5b1d98b4d2aa91e7c4018f2e15b46dafd
iaortiz <38107740+iaortiz@users.noreply.github.com>
2018-11-09 10:09:45 -0500

    Agregando Ejercicio 1

--- ejercicio1/run.py
+++ ejercicio1/run.py
@@ -1,1 +1,7 @@
-
+from mipaquete.modelo import *
+e = Empleado()
+e.agregar_nombre("luis")
+e.agregar_apellido("Benitez")
+e.agregar_cedula("1110001")
+e.agregar_comision_fija(5000)
+print(e.presentar_datos())
